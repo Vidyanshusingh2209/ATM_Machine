1ad7e97e8e239056d16122b29450c9e8b4236b17
Vidyanshu Singh <141514834+Vidyanshusingh2209@users.noreply.github.com>
2023-09-03 23:12:11 +0530

    Update OperationImplement.java

--- src/atm_machine/OperationImplement.java
+++ src/atm_machine/OperationImplement.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package atm_machine;
+package ATM_Machine;
 
-/**
- *
- * @author Vidyanshu
- */
-public class OperationImplement {
-    
+import java.util.HashMap;
+import java.util.Map;
+
+public class OperationImplement implements OperationInterface {
+    ATM atm = new ATM();
+    Map<Double,String> ministmt = new HashMap<>();
+    @Override
+    public void viewBalance() {
+        System.out.println("Available Balance is :" +atm.getBalance());
+        System.out.println();
+        System.out.println();
+    }
+
+    @Override
+    public void withdrawAmount(double withdrawAmount) {
+
+        if (withdrawAmount <= atm.getBalance()) {
+            ministmt.put(withdrawAmount, "Amount Withdrawn");
+
+            System.out.println("Withdrawl Successful...");
+            System.out.println("Collect your Cash: " + withdrawAmount);
+            atm.setBalance(atm.getBalance() - withdrawAmount);
+            viewBalance();
+            System.out.println();
+            System.out.println();
+
+        }else{
+            System.out.println("Insufficient Balance....");
+            System.out.println();
+            System.out.println();
+
+        }
+
+    }
+
+    @Override
+    public void depositAmount(double depositAmount){
+        ministmt.put(depositAmount, "Amount Deposited");
+
+        System.out.println(depositAmount + " Rs. Deposited Successfully...");
+        atm.setBalance(atm.getBalance() + depositAmount);
+        viewBalance();
+        System.out.println();
+        System.out.println();
+
+    }
+
+    @Override
+    public void viewMiniStmt() {
+        for (Map.Entry<Double, String> m:ministmt.entrySet()){
+            System.out.println(m.getKey() + " " + m.getValue());
+        }
+        System.out.println();
+        System.out.println();
+
+
+    }
+
+
 }
+ 
